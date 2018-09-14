c58a61c426f205511d4bf4bf69d820f96e6cab0f
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2018-09-14 10:36:35 +0700

    remove comment

--- server/server.js
+++ server/server.js
@@ -4,23 +4,4 @@ const config = require('./config/env/development');
 var app = require('./config/lib/app');
 
 
-// app.get('/rentals',
-//     (req, res)=>{
-//         res.json({'success': true});
-//     }
-// );
-
 var server = app.start();
-// mongoose.connect(config.DB_URI, { useNewUrlParser: true })
-
-// const app = express();
-
-// app.get('/rentals',
-//     (req, res)=>{
-//         res.json({'success': true});
-//     }
-// );
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, function() {
-//     console.log('im running');
-// });
