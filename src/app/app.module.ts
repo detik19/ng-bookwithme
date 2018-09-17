b9611df7710ad49cb2ce7fee0af59240692ec74a
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2018-09-17 08:23:03 +0700

    add css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 import { RouterModule } from '@angular/router';
 import { BasicLayoutComponent } from './common/basic-layout/basic-layout.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +17,8 @@ import { BasicLayoutComponent } from './common/basic-layout/basic-layout.compone
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
